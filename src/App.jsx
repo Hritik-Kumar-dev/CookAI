7fd060f6aeecf947a9d7cf8a458bc05bbf388437
Hritik-Kumar-dev <hritikkumar.dev77@gmail.com>
2025-06-12 11:00:38 +0530

    created a react setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1>CookAI </h1>
+<p>your ai cook recipe master </p>
+    </>
+  )
+}
+
+export default App
